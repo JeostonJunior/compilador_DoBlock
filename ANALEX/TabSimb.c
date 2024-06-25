27833ee12c529864e032cf001a5eeb4dd5f81a40
Jeoston Araújo <jeostonjunior@gmail.com>
2024-06-25 00:30:09 -0300

    Correção da tabela de simbolos

--- ANALEX/TabSimb.c
+++ ANALEX/TabSimb.c
@@ -1,89 +1,108 @@
 #include "TabSimb.h"
 #include <stdbool.h>
+#include <string.h>
 
+TabIdef tabela_idef;
+
+// Inicializa a tabela de símbolos
 void Iniciar_tabela()
 {
-    tabela_idef.tamTab= 0;
+    tabela_idef.tamTab = 0;
 }
-// considerar o escopo na busca do identificador.
+
+// Busca um identificador pelo escopo na tabela de símbolos
 int Buscar_escopo(char lexema[], int escopo)
 {
-    for (int i = tabela_idef.tamTab - 1; i >= 0; i--) {  // Busca do fim para o início
-        if (tabela_idef.tabela_simb[i].escopo == escopo && strcmp(tabela_idef.tabela_simb[i].lexema, lexema) == 0) {
+    // Busca do fim para o início
+    for (int i = tabela_idef.tamTab - 1; i >= 0; i--)
+    {
+        if (tabela_idef.tabela_simb[i].escopo == escopo && strcmp(tabela_idef.tabela_simb[i].lexema, lexema) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-
-int Buscar_escopo(char lexema[], int escopo)
-{
-    int i;
-    for(i = 0; i < tabela_idef.tamTab;i++){
-        if(!(strcmp(lexema, tabela_idef.tabela_simb[i].lexema))){
-            return i;
-        }
-    }
-    return -1;
-}
-// evitar a inserção duplicada
+// Evita a inserção duplicada na tabela de símbolos
 int Insercao_tabela(char lexema[], int escopo, int tipo, char categoria[TAM_CATEGORIA], bool zombie)
 {
-    if (Buscar_escopo(lexema, escopo) != -1) {
+    if (Buscar_escopo(lexema, escopo) != -1)
+    {
         printf("Erro: Identificador já declarado neste escopo.\n");
         return -1;
     }
 
-    if (tabela_idef.tamTab == TAM_MAX_TAB) {
+    if (tabela_idef.tamTab == TAM_MAX_TAB)
+    {
         printf("Erro: Tabela de símbolos cheia.\n");
         return -1;
-    } else {
+    }
+    else
+    {
         int i = tabela_idef.tamTab;
+
         strcpy(tabela_idef.tabela_simb[i].lexema, lexema);
+
         tabela_idef.tabela_simb[i].endereco = i;
         tabela_idef.tabela_simb[i].escopo = escopo;
         tabela_idef.tabela_simb[i].tipo = tipo;
+
         strcpy(tabela_idef.tabela_simb[i].categoria, categoria);
+
         tabela_idef.tabela_simb[i].zombie = zombie;
         tabela_idef.tamTab++;
+
         return i;
     }
 }
+
+// Remove o último identificador inserido na tabela de símbolos
 int Remover_ultimo()
 {
-    if (tabela_idef.tamTab > 0) {
+    if (tabela_idef.tamTab > 0)
+    {
         int index = tabela_idef.tamTab - 1;
+
         tabela_idef.tabela_simb[index].lexema[0] = '\0';
         tabela_idef.tabela_simb[index].escopo = 0;
         tabela_idef.tabela_simb[index].tipo = 0;
         tabela_idef.tabela_simb[index].categoria[0] = '\0';
         tabela_idef.tabela_simb[index].zombie = false;
         tabela_idef.tamTab--;
+
         return index;
     }
     return -1;
 }
+
+// Busca um identificador na tabela de símbolos
 int Buscar_tabela(char lexema[])
 {
-    for(int i = 0; i < tabela_idef.tabela_simb; i++){
-        if(!(strcmp(lexema,tabela_idef.tabela_simb[i].lexema))){
+    for (int i = 0; i < tabela_idef.tamTab; i++)
+    {
+        if (strcmp(lexema, tabela_idef.tabela_simb[i].lexema) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
-  
-void Imprimir_tabela(TabIdef tabela_idef, int tamTab) {
-    printf("| %-20s | %-10s | %-6s | %-6s | %-10s | %-12s | %-10s | %-14s |\n",
-           "Nome ID", "Endereço", "Escopo", "Tipo", "Categoria", "Constante", "Escalar", "Status Ativo");
-    printf("|----------------------|------------|---------|---------|------------|--------------|------------|------------------|\n");
 
-    for (int i = 0; i < tamTab; i++) {
-        printf("| %-20s | %-10d | %-6d | %-6d | %-10s | %-12s | %-10s | %-14s |\n",
-               tabela_idef.tabela_simb[i].lexema, tabela_idef.tabela_simb[i].endereco,
-               tabela_idef.tabela_simb[i].escopo, tabela_idef.tabela_simb[i].tipo,
-               tabela_idef.tabela_simb[i].categoria, tabela_idef.tabela_simb[i].zombie ? "True" : "False",
-               tabela_idef.tabela_simb[i].endereco ? "True" : "False", tabela_idef.tabela_simb[i].zombie ? "True" : "False");
+void Imprimir_tabela(TabIdef tabela_idef, int tamTab)
+{
+    printf("| %-20s | %-10s | %-6s | %-6s | %-10s | %-6s |\n",
+           "Nome ID", "Endereço", "Escopo", "Tipo", "Categoria", "Zombie");
+    printf("|----------------------|------------|--------|--------|------------|--------|\n");
+
+    for (int i = 0; i < tamTab; i++)
+    {
+        printf("| %-20s | %-10d | %-6d | %-6d | %-10s | %-6s |\n",
+               tabela_idef.tabela_simb[i].lexema,
+               tabela_idef.tabela_simb[i].endereco,
+               tabela_idef.tabela_simb[i].escopo,
+               tabela_idef.tabela_simb[i].tipo,
+               tabela_idef.tabela_simb[i].categoria,
+               tabela_idef.tabela_simb[i].zombie ? "True" : "False");
     }
 }
--- ANALEX/TabSimb.h
+++ ANALEX/TabSimb.h
@@ -1,5 +1,5 @@
 #ifndef TABSIMB
-#define TABIM
+#define TABSIMB
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,35 +7,36 @@
 #include <stdbool.h>
 
 #define TAM_MAX_TAB 100
-#define TAM_CATEGORIA 3 
-typedef
-    struct 
-    {
-        /* data */
-        char lexema[TAM_MAX_LEXEMA];
-        int tipo;
-        int escopo;
-        char categoria[TAM_CATEGORIA];
-        bool zombie;
-        int endereco;
+#define TAM_CATEGORIA 3
 
-    }TabSimb;
-    
-typedef
-    struct 
-    {
-        /* data */
-        int tamTab;
-        TabSimb tabela_simb[TAM_MAX_TAB];
-    }TabIdef;
-    
+// Definição da estrutura TabSimb
+typedef struct
+{
+    char lexema[TAM_MAX_LEXEMA];
+    int tipo;
+    int escopo;
+    char categoria[TAM_CATEGORIA];
+    bool zombie;
+    int endereco;
+} TabSimb;
+
+// Definição da estrutura TabIdef
+typedef struct
+{
+    int tamTab;
+    TabSimb tabela_simb[TAM_MAX_TAB];
+} TabIdef;
+
+// Declaração de variáveis externas
 extern TabIdef tabela_idef;
 extern TOKEN tk;
 
+// Declaração de funções
 void Iniciar_tabela();
 void Imprimir_tabela(TabIdef, int tamTab);
 int Buscar_tabela(char lexema[]);
-int Buscar_escopo(char lexema[],int escopo);
-int Insercao_tabela(char lexema[],int escopo,int tipo, char categoria[TAM_CATEGORIA],bool zombie);
+int Buscar_escopo(char lexema[], int escopo);
+int Insercao_tabela(char lexema[], int escopo, int tipo, char categoria[TAM_CATEGORIA], bool zombie);
 int Remover_ultimo();
+
 #endif
